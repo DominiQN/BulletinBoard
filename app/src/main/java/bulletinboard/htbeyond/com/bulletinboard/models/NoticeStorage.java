960647f05e6a6b64c2fc1be476580b1e903060a5
dominiqn <dmut7691@gmail.com>
2018-09-13 14:34:50 +0900

    NoticeStorage 초기 구성

--- app/src/main/java/bulletinboard/htbeyond/com/bulletinboard/models/NoticeStorage.java
+++ app/src/main/java/bulletinboard/htbeyond/com/bulletinboard/models/NoticeStorage.java
@@ -4,11 +4,12 @@ import android.content.Context;
 
 import java.util.List;
 
-class NoticeStorage {
+public class NoticeStorage {
     private static NoticeStorage sNoticeStorage;
 
     private List<Notice> mNotices;
     private Context mContext;
+    // private HTTP가지고 노는 거 변수
 
     static NoticeStorage getInstance(Context context) {
         if (sNoticeStorage == null) {
@@ -20,4 +21,27 @@ class NoticeStorage {
     private NoticeStorage(Context context) {
         mContext = context.getApplicationContext();
     }
+
+    // 서버에 Notice 추가
+    public boolean addNotice(Notice notice) {
+        return false;
+    }
+
+    public boolean updateNotice(Notice notice) {
+        return false;
+    }
+
+    public boolean deleteNotice(Notice notice) {
+        return false;
+    }
+
+    public Notice getNotice(int noticeNum) {
+        return null;
+    }
+
+    public List<Notice> getNotices() {
+        return null;
+    }
+
+
 }
