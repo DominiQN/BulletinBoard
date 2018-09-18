fa2366ee747f5c217b43b30902309f4cd85fba5f
4256125 <thwjdals12@naver.com>
2018-09-19 01:34:46 +0900

    RecyclerView 갈아 엎기, 게시글 작성 새로 만들기 및 테스트

--- app/src/main/java/bulletinboard/htbeyond/com/bulletinboard/fragments/NoticeListFragment.java
+++ app/src/main/java/bulletinboard/htbeyond/com/bulletinboard/fragments/NoticeListFragment.java
@@ -1,6 +1,5 @@
 package bulletinboard.htbeyond.com.bulletinboard.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,11 +18,10 @@ import org.json.JSONObject;
 
 import java.util.List;
 
-import bulletinboard.htbeyond.com.bulletinboard.NoticeActivity;
-import bulletinboard.htbeyond.com.bulletinboard.NoticeEditActivity;
 import bulletinboard.htbeyond.com.bulletinboard.R;
 import bulletinboard.htbeyond.com.bulletinboard.models.Notice;
 import bulletinboard.htbeyond.com.bulletinboard.models.NoticeStorage;
+import bulletinboard.htbeyond.com.bulletinboard.recyclerviewhelpers.EndlessNoticeAdapter;
 import bulletinboard.htbeyond.com.bulletinboard.recyclerviewhelpers.EndlessRecyclerOnScrollListener;
 import bulletinboard.htbeyond.com.bulletinboard.recyclerviewhelpers.NoticeAdapter;
 import bulletinboard.htbeyond.com.bulletinboard.network.NoticeListJSONWrapper;
@@ -39,12 +37,12 @@ public class NoticeListFragment extends Fragment {
     private static final String KEY_PAGE_NUM
             = "bulletinboard.htbeyond.com.bulletinboard.fragments.page_num";
     private static final String KEY_LAST
-            = "bulletinboard.htbeyond.com.bulletinboard.fragments.last"
+            = "bulletinboard.htbeyond.com.bulletinboard.fragments.last";
 
     private RecyclerView mRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;\
+    private LinearLayoutManager mLinearLayoutManager;
     private EndlessRecyclerOnScrollListener mEndlessRecyclerOnScrollListener;
-    private NoticeAdapter mAdapter;
+    private EndlessNoticeAdapter mAdapter;
     private int mCurrentPageNumber;
     private boolean mIsLast;
 
@@ -68,61 +66,48 @@ public class NoticeListFragment extends Fragment {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.list_recycler_view);
         mLinearLayoutManager = new LinearLayoutManager(getActivity());
 
-        mEndlessRecyclerOnScrollListener =
-                new EndlessRecyclerOnScrollListener() {
-                    @Override
-                    public void onLoadMore(int current_page) {
-                        load
-                    }
-                }
-
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        mRecyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(mLinearLayoutManager) {
             @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-
-                int visibleItemCount = recyclerView.getChildCount();
-                int totalItemCount = mLinearLayoutManager.getItemCount();
-                int firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
-
-                if (mLoading) {
-                    int diffCurrentFromPrevious = totalItemCount - previousItemCount;
-
-                    // check if current total is greater than previous (diff should be greater than 1, for considering placeholder)
-                    // and if current total is equal to the total in server
-                    if ((diffCurrentFromPrevious > 1) ||
-                            totalItemCount >= mTotalEntries) {
-                        mLoading = false;
-                        previousItemCount = totalItemCount;
-                    }
-                } else {
-
-                    if (totalItemCount >= mTotalEntries) {
-                        // do nothing, we've reached the end of the list
-                    } else {
-                        // check if the we've reached the end of the list,
-                        // and if the total items is less than the total items in the server
-                        if ((firstVisibleItem + visibleItemCount) >= totalItemCount &&
-                                totalItemCount < mTotalEntries) {
-                            onLoadMore(++current_page);
-
-                            mLoading = true;
-                            previousItemCount = totalItemCount;
-                        }
-                    }
+            public void onLoadMore(int current_page) {
+                if (mAdapter == null) {
+                    mRecyclerView.setAdapter(new EndlessNoticeAdapter(NoticeStorage.getInstance(getActivity()).getNotices()));
                 }
+                mAdapter = (EndlessNoticeAdapter) mRecyclerView.getAdapter();
+                mAdapter.setIsAppending(true);
+                mAdapter.notifyItemInserted(mAdapter.getItemCount());
 
-            }
-        });
+                Call<JSONObject> res = RetrofitService.getInstance(getActivity()).getService()
+                        .getNotices(getString(R.string.access_token) , NoticeService.NORMAL_SIZE, current_page, NoticeService.MODE_FIND_ALL);
+                res.enqueue(new Callback<JSONObject>() {
+                    @Override
+                    public void onResponse(Call<JSONObject> call, Response<JSONObject> response) {
+                        Log.d(TAG, "getNotices() called" + response.toString());
+                        if (response.isSuccessful()) {
+                            NoticeListJSONWrapper jsonWrapper = new NoticeListJSONWrapper(response.body());
+                            NoticeStorage storage = NoticeStorage.getInstance(getActivity());
+                            if (jsonWrapper.isLast()) {
+                                mIsLast = true;
+                                return;
+                            }
+                            mAdapter.addAll(jsonWrapper.getNotices());
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<JSONObject> call, Throwable t) {
+
+                    }
+                });
+                }
+            });
 
         if (savedInstanceState != null) {
-            mCurrentPageNumber = savedInstanceState.getInt(KEY_PAGE_NUM);
             mIsLast = savedInstanceState.getBoolean(KEY_LAST);
         }
-        mCurrentPageNumber = 0;
 
-        getNotices(30, 0, true);
+        mCurrentPageNumber = 0;
 
         return view;
     }
@@ -130,7 +115,6 @@ public class NoticeListFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        updateUI();
     }
 
     @Override
@@ -141,67 +125,19 @@ public class NoticeListFragment extends Fragment {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_notice_list_item_post:
-                Intent i = NoticeEditActivity.newIntent(getActivity(), NoticeEditActivity.CREATE_NOTICE);
-                startActivity(i);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_notice_list, menu);
     }
 
-    private void updateUI() {
-        List<Notice> notices = NoticeStorage.getInstance(getActivity()).getNotices();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_notice_list_item_post:
 
-        if (mAdapter == null) {
-            mAdapter = new NoticeAdapter(notices);
-            mRecyclerView.setAdapter(mAdapter);
-        } else {
-            mAdapter.setNotices(notices);
-            mAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-    }
-
-    public void getNotices(int pageSize, int pageNum, final boolean isRefresh) {
-
-        Call<JSONObject> res = RetrofitService.getInstance(getActivity()).getService()
-                .getNotices(getString(R.string.access_token) , pageSize, pageNum, NoticeService.MODE_FIND_ALL);
-        res.enqueue(new Callback<JSONObject>() {
-            @Override
-            public void onResponse(Call<JSONObject> call, Response<JSONObject> response) {
-                Log.d(TAG, "getNotices() called" + response.toString());
-                if (response.isSuccessful()) {
-                    NoticeListJSONWrapper jsonWrapper = new NoticeListJSONWrapper(response.body());
-                    NoticeStorage storage = NoticeStorage.getInstance(getActivity());
-                    if (isRefresh) {
-                        storage.setNotices(jsonWrapper.getNotices());
-                    } else {
-                        if (jsonWrapper.isLast()) {
-                            mIsLast = true;
-                            return;
-                        }
-                        mCurrentPageNumber++;
-                        storage.appendNotices(jsonWrapper.getNotices());
-                    }
-
-                    updateUI();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<JSONObject> call, Throwable t) {
-                Log.e(TAG, "getNotices() called" + t.getMessage());
-                showFailToast();
-            }
-        });
     }
 }
